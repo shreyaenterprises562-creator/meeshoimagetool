c94fc5388b0c0458fe4cc1783605063f2d3b0933
Nikunj Goswami <shreyaenterprises562@gmail.com>
2026-03-11 22:48:59 +0530

    fix remove bg script

--- scripts/remove_bg.py
+++ scripts/remove_bg.py
@@ -1,13 +1,21 @@
 from rembg import remove
 from PIL import Image
 import sys
+import io
 
+# arguments
 input_path = sys.argv[1]
 output_path = sys.argv[2]
 
-img = Image.open(input_path).convert("RGBA")
+# read input image
+with open(input_path, "rb") as f:
+    input_data = f.read()
 
-out = remove(img)
+# remove background
+output_data = remove(input_data)
 
-out.save(output_path)
+# save result
+with open(output_path, "wb") as f:
+    f.write(output_data)
+
 print("DONE")
